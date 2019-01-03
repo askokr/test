0cb4211d6456957eda05b4a52868e9a5cebf6c10
Asko Kriiska <asko.kriiska@gmail.com>
2019-01-03 09:01:16 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ImageGallery from './gallery';
 
@@ -20,18 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Galerii test</h1>
         </header>
         <ImageGallery images={images} />
       </div>
